ea60c31b9367290be7d19cbba898c963b1cb947f
Timurs Petrovs <91870903+kdentrsz@users.noreply.github.com>
2022-02-28 08:25:40 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+x = 0
+for i in range(1,11):
+  x += 1
+  c = x ** 2
+  print(c)
+
+
+x = 0
+for i in range(1,16):
+  x += 1
+  z = x ** 3
+  print(z)
+
+x = 0
+for i in range(1,21):
+  x += 1
+  v = x ** 0.5
+  v = round(v, 2)
+  print(v)
